832048ce58e81bd305d04b8ee6074ac2143ec923
Vansh Sharma <xplore.vansh@gmail.com>
2025-07-01 20:24:20 +0530

    Comparision in JS

---
+++ Basics_01/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+
+console.log("2" > 0); 
+
+
+// NOTICE:
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);    // true (Comparision convert null into a number, treating it as 0)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// (===) --> Check comparison and datatype both simultaneously;
+console.log("0" === 0);
