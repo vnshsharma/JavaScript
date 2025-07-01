0ae9302ff73278ec9d8b3d870285aca79e451c0a
Vansh Sharma <xplore.vansh@gmail.com>
2025-07-01 20:33:02 +0530

    Other Data Types

---
+++ Basics_01/other_datatype.js
@@ -0,0 +1,29 @@
+// Reference => Array, Object, functions
+
+// Array 
+const Heros = ["Vansh", "Parth", "Arjun"];
+console.log(Heros);
+
+// Object
+let myObj = {
+    name : "Vansh",
+    age : 18,
+
+}
+console.log(myObj);
+
+// Functions 
+const func = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof(Heros));
+console.log(typeof(myObj));
+console.log(typeof(func));
+
+
+// Memory 
+/* 
+Stack (Primitive) 
+Heap (Non-Primitive)
+*/
