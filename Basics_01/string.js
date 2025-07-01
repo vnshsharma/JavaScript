c762710810f9e185067f41c54ddf4becf25446e8
Vansh Sharma <xplore.vansh@gmail.com>
2025-07-01 20:34:15 +0530

    String

---
+++ Basics_01/string.js
@@ -0,0 +1,16 @@
+const name = "Vansh";
+console.log(name+" Sharma");
+
+// String Interpolation
+console.log(`My name is ${name}`);
+
+// Important Method for declare the string 
+const gameName = new String('Xcs');
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('X'));
