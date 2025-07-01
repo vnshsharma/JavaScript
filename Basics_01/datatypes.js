5fbd0cfe888475b8d36fc06ea2494ace4b31be8f
Vansh Sharma <xplore.vansh@gmail.com>
2025-07-01 20:30:32 +0530

    Data Types

---
+++ Basics_01/datatypes.js
@@ -0,0 +1,19 @@
+let name = "Vansh";  // String
+let age = 18;       // number
+let isLoggedIn = false;      // Boolean
+
+console.log(typeof(age));
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof(null));   // Object 
+console.log(typeof(undefined));   // undefined
